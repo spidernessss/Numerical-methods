3933a054a68638b4624ced6d86c5a5d16eea0fe6
spidernessss <saidodjuana@gmail.com>
2024-04-20 23:26:37 +0300

    Create test_functions.c

---
+++ test_functions.c
@@ -0,0 +1,17 @@
+#ifndef TEST_FUNCTION_C
+#define TEST_FUNCTION_C
+
+double test_func_11(double x)
+{
+    return x + 1;
+}
+double test_func_12(double x)
+{
+    return 1;
+}
+double test_func_13(double x)
+{
+    return -x;
+}
+
+#endif
