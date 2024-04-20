3026fd78363318b3255bda852e43c0e9d05cb8cd
spidernessss <saidodjuana@gmail.com>
2024-04-20 23:26:05 +0300

    Create test_functions.h

---
+++ test_functions.h
@@ -0,0 +1,16 @@
+#ifndef TEST_FUNCTIONS_H
+#define TEST_FUNCTIONS_H
+
+double test_func_11(double x);
+double test_func_12(double x);
+double test_func_13(double x);
+
+double test_func_21(double x);
+double test_func_22(double x);
+double test_func_23(double x);
+
+double test_func_21(double x);
+double test_func_22(double x);
+double test_func_23(double x);
+
+#endif
