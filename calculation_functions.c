08b40ca13a1fec93452b7a783eeaf7e336570a6c
spidernessss <saidodjuana@gmail.com>
2024-05-05 20:27:43 +0300

    Update calculation_functions.c

--- calculation_functions.c
+++ calculation_functions.c
@@ -1,71 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 //!!!!!!!!!!!!!!!!!!!!!!!!!
 //need stdlib.h for abs
 // in my task: root - chord method
-double root(double(*f)(double), double(*g)(double), int a, int b, double eps1)
+double root(double(*f)(double), double(*g)(double), double a, double b, double eps1)
 {
     // will be seeking root for a function F = f - g
     // this root will be the coordinate of functions f and g collision
-    int is_delta_positive = ((f(b) - g(b)) - (f(a) - g(a))) > 0 ? 1 : 0;
-    // for chord method we don't need to look at Jensen's inequality
+    int is_delta_positive = ((f(b) - g(b)) - (f(a) - g(a))) > 0 ? 1 : -1;
+    // Need to look at Jensen's inequality
     // if Der_1 * Der_2 > 0 then go from left to right  (a to b)
     // if Der_1 * Der_2 < 0 then go from right to left   (b to a)
-    int x0;
-    int F_x0;
-    int x;
-    int root;
-    switch(is_delta_positive)
+    double value_at_mid = f((a + b) / 2) - g((a + b) / 2);
+    double mid_value = ( (f(a) - g(a)) + (f(b) - g(b)) ) / 2;
+    int convexity = mid_value > value_at_mid ? 1 : -1;
+    
+    int function_type = convexity * is_delta_positive;
+    
+    int iterations = 0;
+    double x0;
+    double x_mov;
+    double root;
+    switch (function_type)
     {
-        case 1:
+        case -1:
             x0 = a;
-            // formula:
-            // new_x = x - F(x)(pre_x - x)/(F(pre_x) - F(x))
-            F_x0 = a - (((f(a)) - g(a)) * (b - a)) / ((f(b) - g(b)) - (f(a) - g(a)));
+            // getting closer from right to left (a is fixed)
+            x_mov = b;
             do
             {
-                x = F_x0 - ( (f(F_x0) - g(F_x0)) * (x0 - F_x0) ) / ( (f(x0) - g(x0)) -(f(F_x0) - g(F_x0)) );
-                x0 = F_x0;
-                F_x0 = x;
-
-            } while( abs( (f(x + eps1) - g(x + eps1)) - ((f(x) - g(x))) ) >= eps1);
-            // there we have +
-            root = x;
+                x_mov = x_mov - ((f(x_mov) - g(x_mov)) * (x0 - x_mov) ) / ( (f(x0) - g(x0)) - (f(x_mov) - g(x_mov)));
+                iterations++;
+            } while((f(x_mov) - g(x_mov)) * (f(x_mov - eps1) - g(x_mov - eps1)) >= eps1);
+            root = x_mov;
             break;
-        case 2:
+        case 1:
             x0 = b;
-            // formula:
-            // new_x = x - F(x)(pre_x - x)/(F(pre_x) - F(x))
-            F_x0 = b - (((f(b)) - g(b)) * (a - b)) / ((f(a) - g(a)) - (f(b) - g(b)));
+            // getting closer from left to right (b is fixed)
+            x_mov = a;   // moving coordinate
             do
             {
-                x = F_x0 - ( (f(F_x0) - g(F_x0)) * (x0 - F_x0) ) / ( (f(x0) - g(x0)) -(f(F_x0) - g(F_x0)) );
-                x0 = F_x0;
-                F_x0 = x;
-
-            } while( abs( (f(x - eps1) - g(x + eps1)) - ((f(x) - g(x))) ) );
-            //there we have -
-            root = x;
+                x_mov = x0 - ( (f(x0) - g(x0)) * (x_mov - x0) / ((f(x_mov) - g(x_mov)) - (f(x0) - g(x0))) );
+                iterations++;
+            } while((f(x_mov) - g(x_mov)) * (f(x_mov + eps1) - g(x_mov + eps1)) >= eps1 && iterations <= 10 );
+            root = x_mov;
             break;
         default:
             fprintf(stderr, "Error in delta calculation. The values of is_delta_positive: %d\n", is_delta_positive);
     }
-     return root;   
+    printf("Number of iterations in root calculations is %d\n", iterations);
+    return root;   
 }
 // Rectangular method
 // having fixed number of rectangles where step is width and average is length
-double integral(double(*f)(double), int a, int b, double eps2)
+double integral(double(*f)(double), double a, double b, double eps2)
 {
-    int rectangles = 50000;
-    double step = (f(b) - f(a)) / rectangles;
+    // 24.0 is a constant in estimation
+    // 5 is the max value of all function in current interval
+    double step = cbrt(eps2) / (24.0 * 5);
+    double rectangles = (b - a) / step;
     double area = 0;
-    double x0 = 0;
-    double x1 = step;
     for (int i = 0; i < rectangles - 1; i++)
     {
-        area += f((x1 + x0) / 2) * step;
-        x0 += step;
-        x1 += step; 
+        area += ((f(a + i*step) + f(a + (i+1)*step)) / 2) * step;
     }
     return area;
 }
