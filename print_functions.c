2671b67dbd07ce9193bec607cea38ce19ca801b8
spidernessss <saidodjuana@gmail.com>
2024-05-05 20:29:28 +0300

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,20 +4,23 @@
 void print_x_coordinates(void)
 {
     printf("Root's x coordinate of functions #1 and #2 is: %f\n", root(function_1, function_2, 0, 2, 0.00001));
-    printf("Root's x coordinate of functions #2 and #3 is: %f\n", root(function_2, function_3, 0, 2, 0.00001));
+    printf("Root's x coordinate of functions #2 and #3 is: %f\n", root(function_2, function_3, 0.3, 0.9, 0.00001));
     printf("Root's x coordinate of functions #1 and #3 is: %f\n", root(function_1, function_3, -4, -2, 0.00001));
 }
 void print_integral_value(void)
 {
     double root1, root2, root3;
-    root1 = root(function_1, function_2, 0, 2, 0.001);
-    root2 = root(function_2, function_3, 0, 2, 0.001);
-    root3 = root(function_1, function_3, -4, -2, 0.001);
+    root1 = root(function_1, function_2, 0, 2, 0.00001);
+    root2 = root(function_2, function_3, 0.3, 0.9, 0.00001);
+    root3 = root(function_1, function_3, -4, -2, 0.00001);
     double area;
     // Formula: f1[root3, root1] - f2[root1, root2] - f3[root2, root1]
     area = integral(function_1, root3, root1, 0.001);
-    area -= integral(function_2, root1, root2, 0.001);
-    area -= integral(function_3, root2, root1, 0.001);
+  //  printf("Area1: %f\n", area);
+    area -= integral(function_2, root2, root1, 0.001);
+  //  printf("Area2: %f\n", area);
+    area -= integral(function_3, root3, root1, 0.001);
+ //   printf("Area3: %f\n", area);
     printf("The value of area bordered by given three functions is %f.\n", area);
 }
 void print_coordinates_and_integral(void)
