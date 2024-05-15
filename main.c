1fc7a1b3d14f4cb469cadb008440fc84e50cd2c6
spidernessss <saidodjuana@gmail.com>
2024-05-15 15:44:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -24,13 +24,17 @@ int main(int argc, char* argv[])
     fclose(file);
 
     // main arguments interpretation
-    char* command_flags[] = {"-help", "-r", "-i", "-ri", "-q"};
+    char* command_flags[] = {"-help", "-roots", "-square", "-ri", "-iters", "-test-root", "-test-integral"};
     int command_number = 1;
     int flag_index = 0;
+    if (argc == 1)
+    {
+        print_integral_value();
+    }
     while (command_number < argc)
     {
         int was_there_input_error = 1;
-        for (int i = 0; i <= 3; i++)
+        for (int i = 0; i <= 6; i++)
         {
             if (strcmp(argv[command_number], command_flags[i]) == 0 )
             {
@@ -56,6 +60,28 @@ int main(int argc, char* argv[])
                 break;
             case 3:
                 print_coordinates_and_integral();
+                break;
+            case 4:
+                print_iters_roots();
+                break;
+            case 5:
+                int index_func1 = 0; 
+                sscanf(argv[command_number + 1], "%d", &index_func1);
+                int index_func2 = 0;
+                sscanf(argv[command_number + 2], "%d", &index_func2);
+                double a = 0.0;
+                sscanf(argv[command_number + 3], "%lf", &a);
+                double b = 0.0;
+                sscanf(argv[command_number + 4], "%lf", &b);
+                print_test_roots(index_func1, index_func2, a, b);
+                command_number += 4;
+                break;
+            case 6:
+                sscanf(argv[command_number + 1], "%d", &index_func1);
+                sscanf(argv[command_number + 2], "%lf", &a);
+                sscanf(argv[command_number + 3], "%lf", &b);
+                print_test_integral(index_func1, a, b);
+                command_number += 3;
                 break;
         }
         command_number++;
