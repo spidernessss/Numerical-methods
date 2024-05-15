13572915895fc5fcb3ae2fd799becde50274db4b
spidernessss <saidodjuana@gmail.com>
2024-05-15 15:45:44 +0300

    Update print_functions.h

--- print_functions.h
+++ print_functions.h
@@ -1,9 +1,12 @@
 #ifndef PRINT_FUNCTIONS_H
 #define PRINT_FUNCTIONS_H
+void print_iters_roots(void);
 void print_x_coordinates(void);
 void print_integral_value(void);
 void print_coordinates_and_integral(void);
 void print_face_border(void);
 void print_usual_border(void);
 void print_instructions_info(void);
+void print_test_roots(int index_func1, int index_func2, double a, double b);
+void print_test_integral(int index_func1, double a, double b);
 #endif
