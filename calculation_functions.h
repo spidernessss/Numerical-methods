ce1afd354fa6bfbfec6e2bcc25772ee97b855484
spidernessss <saidodjuana@gmail.com>
2024-05-15 15:43:44 +0300

    Update calculation_functions.h

--- calculation_functions.h
+++ calculation_functions.h
@@ -1,7 +1,7 @@
 #ifndef CALCULATION_FUNCTIONS_H
 #define CALCULATION_FUNCTIONS_H
 // in my task: root - chord method
-double root(double(*f)(double), double(*g)(double), double left_border, double right_border, double eps1);
+double root(double(*f)(double), double(*g)(double), double left_border, double right_border, double eps1, int* iterations);
 // in my task integral - rectangular method
 double integral(double(*f)(double), double left_border, double right_border, double eps2);
 #endif
